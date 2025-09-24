73b0061872f4b030607e03b293f8cf8cf096f491
David SPORN <sporniket.studio@gmail.com>
2025-09-24 06:39:54 +0200

    stubed the plugin with empty entry points

---
+++ src/cmake-build-provider.js
@@ -0,0 +1,19 @@
+// TODO -- import e.g. import { CompositeDisposable, Disposable} from 'atom';
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+/****************************************
+
+---
+Copyright (C) 2025 David SPORN
+---
+This is part of **cmake-build-provider-for-pulsar-by-sporniket**.
+A build provider to maintain a list of cmake targets, for Pulsar,
+the community-led, hyper-hackable text editor..
+****************************************/
+
+export default {
+    activate(state) {},
+    deactivate() {},
+    // actions entry points
+    // provided services entry points
+    // consumed services entry points
+};
